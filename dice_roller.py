fb51d19a4564a5e342607421c954037bf85a51ea
th23 <tom.howell23@yahoo.com>
2020-03-27 08:43:43 -0500

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,9 +1,15 @@
 import random
 
 def main():
-    roll = random.randint(1,6)
+    dice_rolls = 2
+    dice_sum = 0
+
+    for i in range(0,dice_rolls):
+        roll = random.randint(1,6)
     #roll=5
-    print(f'You rolled {roll} a die')
+        print(f'You rolled {roll} a die')
+        dice_sum += roll
+    print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
     main()
